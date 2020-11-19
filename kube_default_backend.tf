379a3ef743866890695921fbbaa4e744e9017ca6
Fletcher91 <thom@argu.co>
2020-11-19 11:48:00 +0100

    core#502 Add default backend

---
+++ kube_default_backend.tf
@@ -0,0 +1,78 @@
+resource "kubernetes_deployment" "default-http-backend" {
+  metadata {
+    namespace = var.system_namespace
+    name = "default-http-backend"
+    labels = {
+      k8s-app = "default-http-backend"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        k8s-app = "default-http-backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          k8s-app = "default-http-backend"
+        }
+      }
+
+      spec {
+        termination_grace_period_seconds = 60
+
+        container {
+          image = "gcr.io/google_containers/defaultbackend:1.0"
+          name  = "default-http-backend"
+
+          resources {
+            requests {
+              cpu    = "10m"
+              memory = "20Mi"
+            }
+            limits {
+              cpu    = "10m"
+              memory = "20Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/healthz"
+              port = 8080
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds = 5
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "default-http-backend" {
+  metadata {
+    namespace = var.system_namespace
+    name = "default-http-backend"
+    labels = {
+      k8s-app = "default-http-backend"
+    }
+  }
+
+  spec {
+    port {
+      port = "80"
+      target_port = "8080"
+    }
+
+    selector = {
+      k8s-app = "default-http-backend"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "letsencrypt_email" {
+  type = string
+}
+
+variable "system_namespace" {
+  type = string
+  default = "c66-system"
+}
