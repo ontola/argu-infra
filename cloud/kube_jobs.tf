3a9db17998435ad2760869ff1f9d33010e5f252c
Thom van Kalkeren <thom@argu.co>
2021-03-31 12:02:26 +0200

    core#8 Migrate after apex version change

--- cloud/kube_jobs.tf
+++ cloud/kube_jobs.tf
@@ -71,3 +71,68 @@ resource "kubernetes_job" "clear-cache" {
     }
   }
 }
+
+
+resource "kubernetes_job" "migrate-apex" {
+  wait_for_completion = true
+
+  metadata {
+    name = "migrate-apex-${try(var.service_image_tag[local.job_cache_clear_service], var.image_tag)}"
+  }
+
+  spec {
+    backoff_limit = 4
+
+    template {
+      metadata {
+        labels = {
+          app: var.application_name
+          tier: local.job_cache_clear_service
+          component: "job"
+        }
+      }
+
+      spec {
+        container {
+          name = "migrate-apex-job"
+          image = "${var.image_registry}/${var.image_registry_org}/${var.services.apex.image_name}:${try(var.service_image_tag[local.job_cache_clear_service], var.image_tag)}"
+          command = ["bundle", "exec", "rake", "db:migrate"]
+
+
+          env_from {
+            config_map_ref {
+              name = "wt-configmap-env"
+            }
+          }
+
+          env_from {
+            config_map_ref {
+              name = "wt-configmap-statics"
+            }
+          }
+          env_from {
+            config_map_ref {
+              name = "wt-configmap-env"
+            }
+          }
+          env_from {
+            config_map_ref {
+              name = "wt-configmap-${local.job_cache_clear_service}"
+            }
+          }
+          env_from {
+            secret_ref {
+              name = "wt-secret-${local.job_cache_clear_service}"
+            }
+          }
+          env_from {
+            secret_ref {
+              name = "wt-secret-db-redis"
+            }
+          }
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+}
