797c9a640d85a2cfdba9487b975aacd7938e86d1
Thom <thom@argu.co>
2022-08-30 11:20:00 +0200

    feat: collect all do resources in project

---
+++ cloud/project.tf
@@ -0,0 +1,14 @@
+resource "digitalocean_project" "this" {
+  name        = "${var.organization}-${var.cluster_env}"
+  description = "Terraform managed project. Org: ${var.organization}, app: ${var.application_name}, region: ${var.do_region}, version: ${var.cluster_version}"
+  purpose     = var.application_name
+  environment = var.cluster_env
+
+  resources = concat(
+    values(digitalocean_domain.this)[*].urn,
+    [
+      digitalocean_kubernetes_cluster.k8s-ams3-ontola-apex-1.urn,
+      data.digitalocean_loadbalancer.this.urn,
+    ]
+  )
+}
